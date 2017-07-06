df4362d2b21215e66bb71860c2c3c61e985b1c2f
Chris <pschorr.christian@gmail.com>
2017-07-06 07:37:32 +0200

    Try to install svg-sprite-loader

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,53 @@
 var mix = require('laravel-mix');
 
+// const spriteLoaderPlugin = require('svg-sprite-loader/plugin');
+
 // Autoload jQuery
 // @see: https://github.com/JeffreyWay/laravel-mix/blob/master/docs/autoloading.md
 // mix.autoload({
 // 	jquery: ['$', 'window.jQuery']
 // });
+
+
+// @see: https://www.npmjs.com/package/svg-sprite-loader
+// @see: https://laracasts.com/discuss/channels/laravel/help-needed-svg-sprites-with-laravelmix
+// @see: https://github.com/JeffreyWay/laravel-mix/issues/350
+mix.webpackConfig({
+	// plugins: [
+	// 	new SpriteLoaderPlugin()
+	// ],
+	module: {
+		rules: [
+			// {
+			// 	test:    /\.svg-sprite$/,
+			// 	loader:  'svg-sprite-loader',
+			// 	options: {
+			// 		name:      '[name]_[hash]',
+			// 		prefixize: true
+			// 	}
+			// }
+			// {
+			// 	test:    /\.svg$/,
+			// 	loader:  'svg-sprite-loader',
+			// 	include: path.resolve('./resources/images/tiles/'),
+			// 	options: {
+			// 		// extract:        true,
+			// 		// spriteFilename: 'sprite.svg'
+			// 	}
+			// },
+			{
+				test:    /\.svg$/,
+				loader:  'svg-sprite-loader',
+				options: {
+					runtimeCompat: true
+				}
+			}
+		]
+	}
+	// plugins: [
+	// 	new spriteLoaderPlugin()
+	// ]
+});
 
 /*
  |--------------------------------------------------------------------------
@@ -17,5 +60,5 @@ var mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/cultures.js', 'public/js')
-	.sass('resources/scss/screen.scss', 'public/css')
+// mix.js('resources/js/cultures.js', 'public/js')
+// 	.sass('resources/scss/screen.scss', 'public/css');
