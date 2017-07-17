124364a883890cb33af86a91a35cd2358ce6648e
Christian Pschorr <c.pschorr@datamints.com>
2017-07-17 13:41:12 +0200

    Add scene routing

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,14 @@ $app->get('/', function () use ($app) {
 		'scene' => config('scene/dada57d4-ac8b-4b74-ac70-538046fff862')
 	]);
 });
+
+$app->get('/scene/{uuid}', function ($uuid) use ($app) {
+
+	$app->configure('cultures');
+	$app->configure('scene/' . $uuid);
+
+	return view('index', [
+		'settings' => config('cultures'),
+		'scene' => config('scene/' . $uuid)
+	]);
+});
